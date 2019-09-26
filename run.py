c5b10516c7158f2c070cac3fc39eedaa364bce35
Huy Nguyen <huy9997@gmail.com>
2019-09-25 18:38:44 -0700

    playing the structure of the python project

--- run.py
+++ run.py
@@ -17,8 +17,20 @@ def googleSheetsData():
     client = gspread.authorize(creds)
 
     sheet = client.open('Software_Engineer_Applications').sheet1
-    # get_all_values = sheet.get_all_values()
-    get_row = sheet.row_values(2)
-    return jsonify(get_row)
+    get_all_values = sheet.get_all_values()
+    return jsonify(get_all_values)
+
+@app.route('/mostRecent')
+def getRow():
+    scope = ['https://spreadsheets.google.com/feeds',
+             'https://www.googleapis.com/auth/drive']
+    creds = ServiceAccountCredentials.from_json_keyfile_name(
+        'credentials.json', scope)
+    client = gspread.authorize(creds)
+
+    sheet = client.open('Software_Engineer_Applications').sheet1
+    get_all_values = sheet.get_all_values()
+    get_row = sheet.gridProperties
+    return jsonify(length)
 if __name__ == '__main__':
     app.run()
